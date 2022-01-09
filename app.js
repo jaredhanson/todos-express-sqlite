2b4186108b02e4df763524369f984d0869ca400f
Jared Hanson <jaredhanson@gmail.com>
2022-01-09 15:35:21 -0800

    Display count of items.

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.locals.pluralize = require('pluralize');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
