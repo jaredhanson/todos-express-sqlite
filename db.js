7c2b3f2b859ccfb2c9a2782f6906f3c4b6f011b5
Jared Hanson <jaredhanson@gmail.com>
2022-01-09 14:33:54 -0800

    Initialize database.

---
+++ db.js
@@ -0,0 +1,15 @@
+var sqlite3 = require('sqlite3');
+var mkdirp = require('mkdirp');
+
+mkdirp.sync('var/db');
+
+var db = new sqlite3.Database('var/db/todos.db');
+
+db.serialize(function() {
+  db.run("CREATE TABLE IF NOT EXISTS todos ( \
+    title TEXT NOT NULL, \
+    completed INTEGER \
+  )");
+});
+
+module.exports = db;
