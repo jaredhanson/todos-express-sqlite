8b232415d017a876b10ffb22e9dfe1013398fc49
Jared Hanson <jaredhanson@gmail.com>
2022-01-09 14:51:58 -0800

    Fetch records from DB.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
+
+function fetchTodos(req, res, next) {
+  db.all('SELECT rowid AS id, * FROM todos', [], function(err, rows) {
+    if (err) { return next(err); }
+    
+    var todos = rows.map(function(row) {
+      return {
+        title: row.title,
+        completed: row.completed == 1 ? true : false,
+        url: '/' + row.id
+      }
+    });
+    res.locals.todos = todos;
+    res.locals.activeCount = todos.filter(function(todo) { return !todo.completed; }).length;
+    res.locals.completedCount = todos.length - res.locals.activeCount;
+    next();
+  });
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', fetchTodos, function(req, res, next) {
+  res.render('index');
 });
 
 module.exports = router;
