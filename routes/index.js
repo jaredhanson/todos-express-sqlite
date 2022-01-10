f359f01c2231b0f428cb981ab61fcebf80f0c0e5
Jared Hanson <jaredhanson@gmail.com>
2022-01-10 06:57:28 -0800

    Add active and completed filters.

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,19 @@ function fetchTodos(req, res, next) {
 
 /* GET home page. */
 router.get('/', fetchTodos, function(req, res, next) {
+  res.locals.filter = null;
+  res.render('index');
+});
+
+router.get('/active', fetchTodos, function(req, res, next) {
+  res.locals.todos = res.locals.todos.filter(function(todo) { return !todo.completed; });
+  res.locals.filter = 'active';
+  res.render('index');
+});
+
+router.get('/completed', fetchTodos, function(req, res, next) {
+  res.locals.todos = res.locals.todos.filter(function(todo) { return todo.completed; });
+  res.locals.filter = 'completed';
   res.render('index');
 });
 
